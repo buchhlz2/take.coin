66e68cd7a1e953bd4316e5388dc213b6fcab19ae
Daniel Buchholz <buchhlz2@gmail.com>
2020-09-02 20:30:54 -0700

    Successfully deployed to Kovan testnet

--- networks.js
+++ networks.js
@@ -1,12 +1,33 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 module.exports = {
   networks: {
     development: {
-      protocol: 'http',
-      host: 'localhost',
+      protocol: "http",
+      host: "localhost",
       port: 8545,
       gas: 5000000,
       gasPrice: 5e9,
-      networkId: '*',
+      networkId: "*",
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.mnemonic,
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        ),
+      networkId: 4,
+      gasPrice: 10e9,
+    },
+    kovan: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.METAMASK_DEV_PRIVATE_KEY,
+          `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        ),
+      networkId: 42,
+      gasPrice: 10e9,
     },
   },
 };
---
+++ src/index.js
@@ -0,0 +1,17 @@
+const Web3 = require('web3');
+const { setupLoader } = require('@openzeppelin/contract-loader');
+
+async function main() {
+    const web3 = new Web3('http://localhost:8545');
+    const loader = setupLoader({ provider: web3 }).web3;
+
+    // web3 contract instance -- address of the Taker token contract
+    const address = '0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab';
+    // kovan testnet address: 0xaD3ca0a1A9499B2aD841b2E7b151117b0e6ab53A
+    const taker = loader.fromArtifact('Taker', address);
+    // some basic examples of calling Taker ERC20 methods
+    //const tokenName = await taker.methods.name().call();
+    //const totalSupply = await taker.methods.totalSupply().call();
+}
+
+main();
